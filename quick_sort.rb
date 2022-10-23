7a08fc5d950c35394a8999559895d9fcf1ce0c4d
noguchi999 <noguchi999@gmail.com>
2022-10-23 10:21:24 +0900

    [modify] 完成

--- quick_sort.rb
+++ quick_sort.rb
@@ -1,12 +1,7 @@
-def quick_sort(n, base)
-  if $result.size == 0
-    $result.push base
-  elsif $result[0] >= base
-    $result.unshift base
-  else
-    $result.push base
-  end
+def quick_sort(n)
+  return n if n.size <= 1
 
+  base = n.shift
   lower = []
   upper = []
 
@@ -18,45 +13,10 @@ def quick_sort(n, base)
     end
   end
 
-  if lower.size == 1
-    if $result[0] >= lower[0]
-      $result = lower + $result
-    else
-      $result = $result + lower
-    end
-  end
-
-  if upper.size == 1
-    if $result[0] >= upper[0]
-      $result = upper + $result
-    else
-      $result = $result + upper
-    end
-  end
-  return $result if $result.size == LIMIT
-
-  puts "#{n} #{base} #{lower} #{upper} #{$result} "
-
-  if lower.size > 1
-    new_base = lower.pop
-    quick_sort(lower, new_base)
-  end
-
-  if upper.size > 1
-    new_base = upper.shift
-    quick_sort(upper, new_base)
-  end
-
-  $result
+  quick_sort(lower) + [base] + quick_sort(upper)
 end
 
 if __FILE__ == $0
-  $result = []
-  n = gets.split(" ").map{|x| x.to_i} + [2**64]
-  LIMIT = n.size
-
-  base = n[0]
-  $result = quick_sort(n[1..], base)
-
-  p $result
+  n = gets.split(" ").map{|x| x.to_i}
+  p quick_sort(n)
 end
