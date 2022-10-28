712334d1e636bfde584177ec100461e2025d5812
noguchi999 <noguchi999@gmail.com>
2022-10-28 18:07:03 +0900

    [add] DP 2 Dif: 875 ABC 178 D

---
+++ abc_178_d.rb
@@ -0,0 +1,19 @@
+# DP 3
+if __FILE__ == $0
+  s = gets.to_i
+  mod = 10**9 + 7
+  dp = [0]*(10**5)
+
+  dp[0] = 0
+  dp[1] = 0
+  dp[2] = 0
+  dp[3] = 1
+  dp[4] = 1
+  dp[5] = 1
+  (6..s).each do |i|
+    dp[i] = dp[3..i-3].sum + 1
+    dp[i] %= mod
+  end
+
+  puts dp[s]
+end
