e2b93f36ec5fe76a5c360b05f0065c50f0c61f47
noguchi999 <noguchi999@gmail.com>
2022-11-02 17:59:40 +0900

    [modify]  BFS( 幅優先探索) 3 Dif: 959 ABC 151 D

--- abc_151_d.rb
+++ abc_151_d.rb
@@ -16,10 +16,44 @@ def explore(maze, start_record, start_column)
           que.push [now_record-1, now_column]
         end
       end
+    end
 
+    if 0 <= now_record+1 < H && 0 <= now_column < W
+      if maze[now_record+1][now_column] == "."
+        if maze_count[now_record+1][now_column] == -1
+          maze_count[now_record+1][now_column] = now_count + 1
+          que.push [now_record+1, now_column]
+        end
+      end
+    end
 
+    if 0 <= now_record < H && 0 <= now_column-1 < W
+      if maze[now_record][now_column-1] == "."
+        if maze_count[now_record][now_column-1] == -1
+          maze_count[now_record][now_column-1] = now_count + 1
+          que.push [now_record, now_column-1]
+        end
+      end
+    end
+
+    if 0 <= now_record < H && 0 <= now_column+1 < W
+      if maze[now_record][now_column+1] == "."
+        if maze_count[now_record][now_column+1] == -1
+          maze_count[now_record][now_column+1] = now_count + 1
+          que.push [now_record, now_column+1]
+        end
+      end
     end
   end
+
+  max_count = 0
+  H.times do |r|
+    W.times do |c|
+      max_count = [max_count, maze_count[r][c]].max
+    end
+  end
+
+  max_count
 end
 
 if __FILE__ == $0
@@ -30,4 +64,14 @@ if __FILE__ == $0
     maze.push gets.split.map(&:to_i)
   end
 
+  ans = 0
+  H.times do |r|
+    W.times do |c|
+      if maze[r][c] == "."
+        ans = [ans, explore(maze, r, c)].max
+      end
+    end
+  end
+
+  puts ans
 end
