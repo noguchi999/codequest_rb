17b327968da8f8f90f009eeff5a54ca4793a393a
noguchi999 <noguchi999@gmail.com>
2022-11-06 17:15:40 +0900

    [modify] セグメント ツリー Dif: 1053 ABC 185 F

--- abc_185_f.rb
+++ abc_185_f.rb
@@ -18,6 +18,41 @@ class SegmentTree
       @tree[i] = seg_f(@tree[2**i], @tree[2*i+1])
     end
   end
+
+  def select(k)
+    @tree[k+@num]
+  end
+
+  def update(k, x)
+    i = k + @num
+    @tree[i] = x
+    while i > 1
+      if i%2 = 0
+        @tree[i/2] = @seg_f(@tree[i], @tree[i+1])
+      else
+        @tree[/2] = @seg_f(@tree[i-1], @tree[i])
+      end
+      i /= 2
+    end
+  end
+
+  def query(l, r)
+    result = @init
+    l += @num
+    r += @num + 1
+    while l < r
+      if l%2 == 1
+        result = @seg_f(result, @tree@l)
+        l += 1
+      end
+      if r%2 == 1
+        result = @seg_f(result, @tree[r-1])
+      end
+      l /= 2
+      r /= 2
+    end
+    result
+  end
 end
 
 if __FILE__ == $0
