71b03f41373f599d8daadc68ea03d029cea14729
noguchi999 <noguchi999@gmail.com>
2022-11-13 19:03:50 +0900

    [modify] ダイクストラ法 Dif: 1135 ABC 192 E

--- abc_192_e.rb
+++ abc_192_e.rb
@@ -2,4 +2,12 @@
 if __FILE__ == $0
   N, M, X, Y = gets.split.map(&:to_i)
   connect = Array.new(N+1).map{[]}
+
+  N.times do |i|
+    a, b, t, k = gets.split.map(&:to_i)
+    connect[a].push [b, t, k]
+    connect[b].push [a, t, k]
+  end
+
+  que = []
 end
