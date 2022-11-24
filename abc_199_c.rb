7b924195a6bd77e5563883137f994e8944b372ab
noguchi999 <noguchi999@gmail.com>
2022-11-24 18:48:14 +0900

    [add] 文字列操作2 Dif:436 ABC199 C

---
+++ abc_199_c.rb
@@ -0,0 +1,14 @@
+# 文字列操作2 Dif:436 ABC199 C
+if __FILE__ == $0
+  N = gets.split.map(&:to_i)
+  S = gets
+  Q = gets.split.map(&:to_i)
+
+  s = "0#{S}"
+  s = [s]
+
+  flip = false
+  Q.times do |i|
+    t, a, b = gets.split.map(&:to_i)
+  end
+end
