3b43d72b3f42cd916fe0f98604bd3b7ad3a3c765
noguchi999 <noguchi999@gmail.com>
2022-12-28 13:26:00 +0900

    [modify] DP1 ABC211 C Dif:559

--- abc_211_c.rb
+++ abc_211_c.rb
@@ -7,5 +7,21 @@ if __FILE__ == $0
   t = "?chokudai"
   mod = 10**9+7
 
+  dp = Array.new(N+1).map{[0]*9}
+  (N+1).times do |i|
+    dp[i][0] = 1
+  end
 
+  (1..N).each do |i|
+    (1..8).each do |k|
+      if s[i] == t[k]
+        dp[i][k] = dp[i-1][k] + dp[i-1][k-1]
+      else
+        dp[i][k] = dp[i-1][k]
+      end
+    end
+    dp[i][k] %= mod
+  end
+
+  puts dp[N][8]
 end
