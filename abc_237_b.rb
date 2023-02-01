ee94f966bcc95916992b28ec19cbecab05981056
noguchi999 <noguchi999@gmail.com>
2023-02-01 18:13:03 +0900

    [modify] ABC237 B

--- abc_237_b.rb
+++ abc_237_b.rb
@@ -6,4 +6,15 @@ if __FILE__ == $0
     a_tmp = gets.split.map(&:to_i)
     A.push a_tmp
   end
+
+  B = Array.new(W).map{[0*H]}
+  W.times do |i|
+    H.times do |j|
+      B[i][j] = A[j][i]
+    end
+  end
+
+  W.times do |i|
+    puts B[i]
+  end
 end
