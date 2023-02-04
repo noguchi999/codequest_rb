ee7c6cd08831f5c94c139e9d04a88b2653159143
noguchi999 <noguchi999@gmail.com>
2023-02-04 14:14:43 +0900

    [modify] ABC237 C

--- abc_237_c.rb
+++ abc_237_c.rb
@@ -12,4 +12,27 @@ if __FILE__ == $0
   end
 
   a_back = 0
+  S.size.times.reverse_each do |i|
+    if S[i] == "a"
+      a_back += 1
+    else
+      break
+    end
+  end
+
+  if a_back < a_front;
+    puts "No"
+    exit
+  end
+
+  _s = "a"*(a_back - a_front) + S
+  N = _s.size
+  N.times do |i|
+    if S[i] != S[N-i-1]
+      puts "No"
+      exit
+    end
+  end
+
+  puts "Yes"
 end
