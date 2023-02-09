6e0949caefa9ad5a874afab108cbdb51f6885e3b
noguchi999 <noguchi999@gmail.com>
2023-02-09 16:56:09 +0900

    [modify] ABC238 D

--- abc_238_d.rb
+++ abc_238_d.rb
@@ -1,6 +1,10 @@
 # ABC238 D
 if __FILE__ == $0
-  N = gets.split.map(&:to_i)
-  N.times do |t|
+  n = gets.split.map(&:to_i)
+  n.times do |t|
+    a, s = gets.split.map(&:to_i)
+    up = 0
+    ans = "Yes"
+    n = [a.bit_length, s.bit_length].max
   end
 end
