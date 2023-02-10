6480f5359bb22b9f14522560887935f78df59147
noguchi999 <noguchi999@gmail.com>
2023-02-10 19:40:51 +0900

    [modify] BC238 D

--- abc_238_d.rb
+++ abc_238_d.rb
@@ -6,5 +6,29 @@ if __FILE__ == $0
     up = 0
     ans = "Yes"
     n = [a.bit_length, s.bit_length].max
+
+    n.times do |i|
+      ai = a>>i & 1
+      si = s>>i & 1
+      if ai == 0
+        if up == 1 && si == 0
+          up = 1
+        else
+          up = 0
+        end
+      else
+        if up + si == 1
+          ans = "No"
+          break
+        end
+        up = 1
+      end
+
+      if i == n - 1 && up == 1
+        ans = "No"
+      end
+    end
   end
+
+  puts ans
 end
