4dafea4d770b23537bec2b2df9df143172dc7323
noguchi999 <noguchi999@gmail.com>
2023-02-17 19:45:47 +0900

    [modify] ABC239 D

--- abc_239_d.rb
+++ abc_239_d.rb
@@ -1,8 +1,33 @@
 # ABC239 D
 def eratosthenes(N)
   is_prime = [true]*(N+1)
+  i = 2
+  while i**2 <= N
+    unless is_prime[i]
+      i += 1
+      next
+    end
+
+    k = 2
+    while i*k <=N
+      is_prime[i*k] = false
+      k += 1
+    end
+
+    i += 1
+  end
+
+  p_list = []
+  (2..N).each do |i|
+    if is_prime[i]
+      p_list.push i
+    end
+  end
+  p_list
 end
 
 if __FILE__ == $0
   a, b, c, d = gets.split.map(&:to_i)
+
+  _p = eratosthenes(200)
 end
