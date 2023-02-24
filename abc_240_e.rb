a6b34a2bc406dff116ddcca988d863fc027f5f7c
noguchi999 <noguchi999@gmail.com>
2023-02-24 17:52:30 +0900

    [modify] ABC240 E

--- abc_240_e.rb
+++ abc_240_e.rb
@@ -1,4 +1,14 @@
 # ABC240 E
 if __FILE__ == $0
   N = gets.split.map(&:to_i)
+  edge = Array.new(N+1).map{[]}
+  (N-1).times do |i|
+    u, v = gets.split.map(&:to_i)
+    edge[u].push v
+    edge[v].push u
+  end
+
+  lr = Array.new(N+1).map{[10**9, 1]}
+  que = [[1, 0, 1]]
+  leaf_count = 1
 end
