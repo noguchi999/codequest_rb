5c7e7decccae1c0ba310d7b494bc3464fb31664f
noguchi999 <noguchi999@gmail.com>
2023-03-05 09:35:47 +0900

    [modify] ABC241 D

--- abc_241_d.rb
+++ abc_241_d.rb
@@ -22,6 +22,27 @@ def nibutan_left(x_index, k)
   end
 end
 
+def nibutan_right(x_index, k)
+  l = x_index
+  r = N - 1
+  while 1 < r - l
+    m = (l + r) / 2
+    if k <= FT.sum(x_index, m)
+      r = m
+    else
+      l = m
+    end
+  end
+
+  if k <= FT.sum(x_index, l)
+    l
+  elsif k <= FT.sum(x_index, r)
+    r
+  else
+    -1
+  end
+end
+
 if __FILE__ == $0
   Q = gets.split.map(&:to_i)
 
@@ -41,4 +62,28 @@ if __FILE__ == $0
   end
 
   FT = FenwickTree.new(N)
+  Q.times do |i|
+    if queries[i][0] == 1
+      x = queries[i][1]
+      FT.add(conv[x], 1)
+    elsif queries[i][0] == 2
+      x = queries[i][1]
+      k = queries[i][2]
+      if FT.sum(0, conv[x]) < k
+        puts -1
+      else
+        m = nibutan_left(conv[x], k)
+        puts _set[m]
+      end
+    else
+      x = queries[i][1]
+      k = queries[i][2]
+      if FT.sum(conv[x], N - 1) < k
+        puts -1
+      else
+        m = nibutan_right(conv[x], k)
+        puts _set[m]
+      end
+    end
+  end
 end
