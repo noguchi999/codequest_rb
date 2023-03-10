a45bcdace6081ad8e84d808b9bb68275dfa3232b
noguchi999 <noguchi999@gmail.com>
2023-03-11 07:22:56 +0900

    [add] ABC243 A

--- abc_243_a.rb
+++ abc_243_a.rb
@@ -1,4 +1,23 @@
 # ABC243 A
 if __FILE__ == $0
   V, A, B, C = gets.split.map(&:to_i)
+  (10**10).times do |i|
+    V -= A
+    if V <= 0
+      puts "F"
+      exit
+    end
+  end
+
+  V -= B
+  if V < 0
+    puts "M"
+    exit
+  end
+
+  V -= C
+  if V < 0
+    puts "T"
+    exit
+  end
 end
