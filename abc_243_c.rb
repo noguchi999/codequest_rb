8540c6c257601a43b4a353fc69ab74b62a098633
noguchi999 <noguchi999@gmail.com>
2023-03-14 19:12:56 +0900

    [modify] ABC243 C

--- abc_243_c.rb
+++ abc_243_c.rb
@@ -1,5 +1,16 @@
+require 'set'
+
 # ABC243 C
 if __FILE__ == $0
   N = gets.split.map(&:to_i)
   _p = []
+  y_set = Set.new
+  N.times do |i|
+    X, Y = gets.split.map(&:to_i)
+    _p << [X, Y]
+    y_set << Y
+  end
+
+  S = gets
+
 end
