03b1c4787f4107755036fea42a740c46d4e4e4b8
noguchi999 <noguchi999@gmail.com>
2023-03-22 21:18:11 +0900

    [modify] ABC244 B

--- abc_244_B.rb
+++ abc_244_B.rb
@@ -4,6 +4,32 @@ if __FILE__ == $0
   T = gets.chomp
 
   direct = "East"
-  X = 0
-  Y = 0
+  x = 0
+  y = 0
+
+  N.times do |i|
+    if T[i] == "S":
+      if direct == "East":
+        x += 1
+      elsif direct == "South":
+        y -= 1
+      elsif direct == "West":
+        x -= 1
+      elsif direct == "North":
+        y += 1
+      end
+    else
+      if direct == "East":
+        direct = "South"
+      elsif direct == "South":
+        direct = "West"
+      elsif direct == "West":
+        direct = "North"
+      elsif direct == "North":
+        direct = "East"
+      end
+    end
+  end
+
+  puts "#{x} #{y}"
 end
