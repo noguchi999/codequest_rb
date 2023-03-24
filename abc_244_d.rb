ecb5e632962588477b8ffa28d1b6020ac1e3bed1
noguchi999 <noguchi999@gmail.com>
2023-03-24 20:09:33 +0900

    [modify] ABC244 D

--- abc_244_d.rb
+++ abc_244_d.rb
@@ -2,4 +2,12 @@
 if __FILE__ == $0
   S1, S2, S3 = gets.chomp.split
   T1, T2, T3 = gets.chomp.split
+
+  if S1 == T1 && S2 == T2 && S3 == T3
+    puts 'Yes'
+  elsif S1 == T1 || S2 == T2 || S3 == T3
+    puts 'No'
+  else
+    puts 'Yes'
+  end
 end
