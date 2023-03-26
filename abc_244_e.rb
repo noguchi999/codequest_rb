8733d11a9d6fb04cbed07b348db7224447794ca8
noguchi999 <noguchi999@gmail.com>
2023-03-26 10:50:00 +0900

    [modify] ABC244 E

--- abc_244_e.rb
+++ abc_244_e.rb
@@ -10,4 +10,30 @@ if __FILE__ == $0
   end
 
   dp[0][S][0] = 0
+  (1..K).each do |i|
+    edge.each do |u, v|
+      if v == X
+        dp[i][v][0] += dp[i-1][u][1]
+        dp[i][v][1] += dp[i-1][u][0]
+      else
+        dp[i][v][0] += dp[i-1][u][0]
+        dp[i][v][1] += dp[i-1][u][1]
+      end
+      dp[i][v][0] %= mod
+      dp[i][v][1] %= mod
+
+      if u == X
+        dp[i][u][0] += dp[i-1][v][1]
+        dp[i][u][1] += dp[i-1][v][0]
+      else
+        dp[i][u][0] += dp[i-1][v][0]
+        dp[i][u][1] += dp[i-1][v][1]
+      end
+
+      dp[i][u][0] %= mod
+      dp[i][u][1] %= mod
+    end
+  end
+
+  puts dp[K][T][0]
 end
