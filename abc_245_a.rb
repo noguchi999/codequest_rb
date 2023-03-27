9ae2e4b6db67b2306f52cd4704a82d674dca129a
noguchi999 <noguchi999@gmail.com>
2023-03-27 18:14:58 +0900

    [modify] ABC245 A

---
+++ abc_245_a.rb
@@ -0,0 +1,15 @@
+# ABC245 A
+if __FILE__ == $0
+  A, B, C, D = gets.chomp.split.map(&:to_i)
+  if A > c
+    puts "Takahashi"
+  elsif C < A
+    puts "Aoki"
+  else
+    if B <= D
+      puts "Takahashi"
+    else
+      puts "Aoki"
+    end
+  end
+end
