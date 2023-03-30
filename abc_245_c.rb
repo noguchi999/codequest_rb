08e23dc3e00ef16a5a6af9181ffbd8e1696bc1ea
noguchi999 <noguchi999@gmail.com>
2023-03-30 18:02:24 +0900

    [modify] ABC245 C

--- abc_245_c.rb
+++ abc_245_c.rb
@@ -3,4 +3,8 @@ if __FILE__ == $0
   N, K = gets.split.map(&:to_i)
   A = [0] + gets.split.map(&:to_i)
   B = [0] + gets.split.map(&:to_i)
+
+  dp = Array.new(2) { Array.new(N+1, false) }
+  dp[0][1] = true
+  dp[1][1] = true
 end
