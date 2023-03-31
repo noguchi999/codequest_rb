1c2a8a8d35872db681f31dd2bf0bfd23c3da90ba
noguchi999 <noguchi999@gmail.com>
2023-03-31 18:14:11 +0900

    [modify] ABC245 C

--- abc_245_c.rb
+++ abc_245_c.rb
@@ -7,4 +7,31 @@ if __FILE__ == $0
   dp = Array.new(2) { Array.new(N+1, false) }
   dp[0][1] = true
   dp[1][1] = true
+  (1..N).each do |i|
+    if dp[0][i]
+      if (A[i] - A[i+1]).abs <= K
+        dp[0][i+1] = true
+      end
+
+      if (A[i] - B[i+1]).abs <= K
+        dp[1][i+1] = true
+      end
+    end
+
+    if dp[1][i]
+      if (B[i] - A[i+1]).abs <= K
+        dp[0][i+1] = true
+      end
+
+      if (B[i] - B[i+1]).abs <= K
+        dp[1][i+1] = true
+      end
+    end
+  end
+
+  if dp[0][N] || dp[1][N]
+    puts "YES"
+  else
+    puts "NO"
+  end
 end
