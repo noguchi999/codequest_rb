383c5bbc00c67c703fc40e3ef7aa12cd64f535ee
noguchi999 <noguchi999@gmail.com>
2023-04-06 19:34:12 +0900

    [modify] ABC247 B

--- abc_247_b.rb
+++ abc_247_b.rb
@@ -3,4 +3,27 @@ if __FILE__ == $0
   N = gets.to_i
   s_list = []
   t_list = []
+
+  N.times do |i|
+    s, t = gets.split
+    s_list << s
+    t_list << t
+  end
+
+  N.times do |i|
+    other_name_list = []
+    N.times do |j|
+      if i != j
+        other_name_list << s_list[j]
+        other_name_list << t_list[j]
+      end
+    end
+
+    if other_name_list.include?(s_list[i]) && other_name_list.include?(t_list[i])
+      puts "No"
+      exit
+    end
+  end
+
+  puts "Yes"
 end
