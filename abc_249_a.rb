fb665da5dae2ae043b3d48b0f11b64b2283ab311
noguchi999 <noguchi999@gmail.com>
2023-04-14 18:51:07 +0900

    [modify] ABC249 A

--- abc_249_a.rb
+++ abc_249_a.rb
@@ -4,4 +4,16 @@ if __FILE__ == $0
 
   _time = X
   taka = 0
+  while 0 < _time
+    if A <= _time
+      taka += A * B
+      _time -= A + C
+    else
+      taka += _time * B
+      _time = 0
+    end
+  end
+
+  _time = X
+  aoki = 0
 end
