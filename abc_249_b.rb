c300797327b84dcd55aa27a479382905211ced49
noguchi999 <noguchi999@gmail.com>
2023-04-18 14:51:23 +0900

    ABC249 B

--- abc_249_b.rb
+++ abc_249_b.rb
@@ -4,4 +4,12 @@ if __FILE__ == $0
 
   is_upper_case = false
   is_lower_case = false
+
+  S.each_char do |c|
+    if c == c.upcase
+      is_upper_case = true
+    else
+      is_lower_case = true
+    end
+  end
 end
