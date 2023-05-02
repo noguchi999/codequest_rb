1c20dbb521410575f745f8756dea5d2fecadc4b4
noguchi999 <noguchi999@gmail.com>
2023-05-02 17:26:37 +0900

    [modify] ABC250 A

--- abc_250_a.rb
+++ abc_250_a.rb
@@ -7,4 +7,18 @@ if __FILE__ == $0
   if 1 <= R - 1
     ans += 1
   end
+
+  if R + 1 <= H
+    ans += 1
+  end
+
+  if 1 <= C - 1
+    ans += 1
+  end
+
+  if C + 1 <= W
+    ans += 1
+  end
+
+  puts ans
 end
