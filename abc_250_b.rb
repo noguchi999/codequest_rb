809369b1b94f77daa2fb8050c236045c621eebde
noguchi999 <noguchi999@gmail.com>
2023-05-05 08:36:31 +0900

    [modify] ABC250 B

--- abc_250_b.rb
+++ abc_250_b.rb
@@ -1,4 +1,19 @@
 # ABC250 B
 if __FILE__ == $0
   N, A, B = gets.split.map(&:to_i)
+  (1..N).each do |i|
+    if i%2 == 1
+      (1..A).each do |j|
+        line = ""
+        (1..N).each do |k|
+          if k %2 == 1
+            line += "." * B
+          else
+            line += "#" * B
+          end
+        end
+        puts line
+      end
+    else
+
 end
