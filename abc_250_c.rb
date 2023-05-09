ffb542c0a17ee25004e1b6175c56812ac24ac962
noguchi999 <noguchi999@gmail.com>
2023-05-09 18:19:47 +0900

    [modify] ABC250 C

--- abc_250_c.rb
+++ abc_250_c.rb
@@ -3,4 +3,8 @@ if __FILE__ == $0
   N, A, B = gets.split.map(&:to_i)
   A = Array.new(N+1).map.with_index {|x, i| i}
   index = Array.new(N+1).map.with_index {|x, i| i}
+
+  Q.times do |i|
+    x = gets.split.map(&:to_i)
+  end
 end
