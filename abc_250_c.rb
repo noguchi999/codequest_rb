996f483b2c0b2efa97764b74c2c0f135b5fadb9d
noguchi999 <noguchi999@gmail.com>
2023-05-12 14:32:29 +0900

    [modify] ABC250 C

--- abc_250_c.rb
+++ abc_250_c.rb
@@ -11,7 +11,16 @@ if __FILE__ == $0
     right_x = nil
     if x_index != N
       right_x = A[x_index+1]
-
-
+      A[x_index], A[x_index+1] = A[x_index+1], A[x_index]
+      index[x] = x_index
+      index[right_x] = x_index+1
+    else
+      left_x = A[x_index-1]
+      A[x_index], A[x_index-1] = A[x_index-1], A[x_index]
+      index[x] = x_index - 1
+      index[left_x] = x_index
+    end
   end
+
+  puts A[1..-1]
 end
