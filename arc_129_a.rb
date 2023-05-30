06722f07c78bf2d5fda30d3e78c852bec3f8f78c
noguchi999 <noguchi999@gmail.com>
2023-05-30 18:11:18 +0900

    [modify] ARC129 A

--- arc_129_a.rb
+++ arc_129_a.rb
@@ -1,4 +1,13 @@
 # ARC129 A
 if __FILE__ == $0
   N, L, R = gets.chomp.split().map(&:to_i)
+  lk = 0
+  while 2**(lk+1) <= L
+    lk += 1
+  end
+
+  rk = 0
+  while 2**(rk+1) <= R
+    rk += 1
+  end
 end
