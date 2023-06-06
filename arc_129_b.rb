52810a28ff655530d3d8eb1d98d64b4fb39b4048
noguchi999 <noguchi999@gmail.com>
2023-06-06 16:36:51 +0900

    [modify]  ARC129 B

--- arc_129_b.rb
+++ arc_129_b.rb
@@ -3,4 +3,10 @@ if __FILE__ == $0
   N = gets.chomp.split().map(&:to_i)
   l_max = 0
   r_min = 10**10
+
+  N.times do |k|
+    l, r = gets.chomp.split().map(&:to_i)
+    l_max = [l, l_max].max
+    r_min = [r, r_min].min
+  end
 end
