705658983bf0f849fdb25bc1aae2962cc050476d
noguchi999 <noguchi999@gmail.com>
2023-06-11 12:24:52 +0900

    [modify] ARC130 A

--- arc_130_a.rb
+++ arc_130_a.rb
@@ -6,4 +6,14 @@ if __FILE__ == $0
   s_list = []
   now = S[0]
   count = 1
+  (1..N-1).each do |i|
+    if S[i] == now
+      count += 1
+    else
+      s_list.push([now, count])
+      now = S[i]
+      count = 1
+    end
+  end
+
 end
