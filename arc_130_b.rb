9464958877ee63d575baed604330c8661007b0e3
noguchi999 <noguchi999@gmail.com>
2023-06-14 17:12:29 +0900

    ARC130 B

--- arc_130_b.rb
+++ arc_130_b.rb
@@ -1,4 +1,10 @@
 # ARC130 B
 if __FILE__ == $0
   H, W, C, Q = gets.chomp.split().map(&:to_i)
+
+  queries = []
+  Q.times do |i|
+    t, n, c = gets.chomp.split().map(&:to_i)
+    queries << [t, n, c]
+  end
 end
