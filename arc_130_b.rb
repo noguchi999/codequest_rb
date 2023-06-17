51ef84242641e834a9090395ed9dbf3416510d5c
noguchi999 <noguchi999@gmail.com>
2023-06-17 21:34:19 +0900

    [modify] ARC130 B

--- arc_130_b.rb
+++ arc_130_b.rb
@@ -15,4 +15,21 @@ if __FILE__ == $0
   col = 0
 
   queries = queries.reverse
+  queries.each do |t, n, c|
+    if t == 1
+      if row_colors[n] == 0
+        colors[c] += W - col
+        row_colors[n] = 1
+        row += 1
+      end
+    else
+      if col_colors[n] == 0
+        colors[c] += H - row
+        col_colors[n] = 1
+        col += 1
+      end
+    end
+  end
+
+  puts colors[1..-1].join("\n")
 end
