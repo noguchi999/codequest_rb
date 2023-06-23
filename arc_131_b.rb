9c00a7ff0e09e278243816b0d3ffb1a99ec16959
noguchi999 <noguchi999@gmail.com>
2023-06-23 16:37:34 +0900

    [modify] ARC131 B

--- arc_131_b.rb
+++ arc_131_b.rb
@@ -1,5 +1,15 @@
 # ARC131 B
 def color(r, c)
+  used = Set.new
+  used << grid[r-1][c] if 0 <= r-1
+  used << grid[r+1][c] if r+1 < H
+  used << grid[r][c-1] if 0 <= c-1
+  used << grid[r][c+1] if c+1 < W
+  return "1" unless used.include? "1"
+  return "2" unless used.include? "2"
+  return "3" unless used.include? "3"
+  return "4" unless used.include? "4"
+  return "5" unless used.include? "5"
 end
 
 if __FILE__ == $0
