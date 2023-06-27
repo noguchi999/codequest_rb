660c5f0c2171da49507c5752ffe0e667cb0b74f2
noguchi999 <noguchi999@gmail.com>
2023-06-27 16:02:16 +0900

    [modify] ARC131 C

--- arc_131_c.rb
+++ arc_131_c.rb
@@ -7,4 +7,14 @@ if __FILE__ == $0
   A.times do |x|
     axor ^= x
   end
+
+  if N%2 == 1
+    puts "win"
+  else
+    if axor in A
+      puts "win"
+    else
+      puts "lose"
+    end
+  end
 end
