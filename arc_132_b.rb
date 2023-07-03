d0b45c5832d9e350e591f73575abb8e46e7925aa
noguchi999 <noguchi999@gmail.com>
2023-07-04 06:04:47 +0900

    [modify] ARC132 B

--- arc_132_b.rb
+++ arc_132_b.rb
@@ -5,4 +5,14 @@ if __FILE__ == $0
 
   index_1 = _p.index(1)
   index_n = _p.index(n)
+
+  if index_1 == 0 && index_n == n - 1
+    puts 0
+    exit
+  elsif index_1 == n - 1 && index_n == 0
+    puts 1
+    exit
+  end
+
+  ans = 10**10
 end
