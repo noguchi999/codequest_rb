478bb77b732aa429899bd5bd225e3d50bff79a08
noguchi999 <noguchi999@gmail.com>
2023-07-19 05:35:33 +0900

    [modify] ARC134 A

--- arc_134_a.rb
+++ arc_134_a.rb
@@ -10,4 +10,14 @@ end
 if __FILE__ == $0
   N, L, W = gets.split.map(&:to_i)
   a = gets.split.map(&:to_i)
+
+  ans = ceil(a[0], W)
+  (N-1).times do |i|
+    if a[i] + W < a[i+1]
+      ans += ceil(a[i+1] - (a[i] + W), W)
+    end
+  end
+
+  ans += ceil((a[i+1] - (a[i] + W)), W)
+  puts ans
 end
