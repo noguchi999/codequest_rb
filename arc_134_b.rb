9aeb11f2571af1667005b4378b7419544e670cbc
noguchi999 <noguchi999@gmail.com>
2023-07-22 05:25:56 +0900

    [modify] ARC134 B

--- arc_134_b.rb
+++ arc_134_b.rb
@@ -1,4 +1,15 @@
 # ARC134 B
+def target(count)
+  (97..122).each do |code|
+    alphabet = code.chr
+    if 1 <= count[alphabet]
+      return alphabet
+    end
+  end
+
+  return nil
+end
+
 if __FILE__ == $0
   N = gets.to_i
   s = gets
