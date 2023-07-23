1862d689687677303b08470481d2090986815ee9
noguchi999 <noguchi999@gmail.com>
2023-07-24 08:46:04 +0900

    [modify] ARC134 B

--- arc_134_b.rb
+++ arc_134_b.rb
@@ -24,4 +24,24 @@ if __FILE__ == $0
   l = 0
   r = N - 1
   x = target(count)
+
+  while l < r
+    if s[l] == x
+      count[s[l]] -= 1
+      l += 1
+      x = target(count)
+    elsif s[r] == target(count)
+      count[s[r]] -= 1
+      count[s[l]] -= 1
+      s[r], s[l] = s[l], s[r]
+      l += 1
+      r -= 1
+      x = target(count)
+    else
+      count[s[r]] -= 1
+      r -= 1
+    end
+  end
+
+  puts s.join
 end
