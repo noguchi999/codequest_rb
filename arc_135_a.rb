1000a9b4a93eaee0a7244627a5e1e42a5454e265
noguchi999 <noguchi999@gmail.com>
2023-07-27 07:36:46 +0900

    [modify] ARC135 A

--- arc_135_a.rb
+++ arc_135_a.rb
@@ -6,4 +6,20 @@ if __FILE__ == $0
   count = Hash.new(0)
   count[X] = 1
   num_set = Set.new([X])
+
+  while 4 < num_set.max
+    tmp_set = Set.new
+    num_set.each do |_p|
+      if 4 < _p
+        if _p%2 == 0
+          count[_p/2] += 2 * count[_p]
+          tmp_set.add(_p/2)
+        elsif _p%2 == 1
+          count[_p/2] += count[_p]
+          count[_p/2 + 1] += count[_p]
+          tmp_set.add(_p/2)
+          tmp_set.add(_p/2 + 1)
+        end
+      end
+    end
 end
