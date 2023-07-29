d6a4aacb6d0c99bc555e6fbe658e874fad3f5c3e
noguchi999 <noguchi999@gmail.com>
2023-07-30 05:50:06 +0900

    [modify] ARC136 A

--- arc_136_a.rb
+++ arc_136_a.rb
@@ -1,5 +1,15 @@
 # ARC136 A
 if __FILE__ == $0
   N = gets.split.map(&:to_i)
-  S = gets.chomp
+  S = gets.chomp.split(//)
+
+  (N-1).times do |i|
+    if S[i] == "B" && S[i+1] == "A"
+      S[i] = "A"
+      S[i+1] = "B"
+    elsif S[i] == "A" && S[i+1] == "B"
+      S[i] = "A"
+      S[i+1] = "X"
+    end
+  end
 end
