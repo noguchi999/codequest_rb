bb26997e6e70caf5cbc856b224d4c6eb28a3464b
noguchi999 <noguchi999@gmail.com>
2023-08-02 06:36:00 +0900

    [modify] ARC137 A

--- arc_137_a.rb
+++ arc_137_a.rb
@@ -1,4 +1,17 @@
 # ARC137 A
 if __FILE__ == $0
   L, R = gets.split.map(&:to_i)
+
+  (L..L+999).each do |l|
+    (R..R-999).reverse_each do |r|
+      if l < r
+        if l.gcd(r) == 1
+          ans = (r-l, ans).max
+        end
+      else
+        break
+      end
+    end
+  end
+  puts ans
 end
