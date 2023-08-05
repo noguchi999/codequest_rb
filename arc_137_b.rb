95bbe4a642e29478b6e5698a987fd11158e4a5a9
noguchi999 <noguchi999@gmail.com>
2023-08-05 16:52:46 +0900

    [modify] ARC137 B

--- arc_137_b.rb
+++ arc_137_b.rb
@@ -3,4 +3,12 @@ if __FILE__ == $0
   N = gets.to_i
   A = [0] + gets.split.map(&:to_i)
   C = [0] * (N + 1)
+
+  (1..N).each do |i|
+    if A[i] == 1
+      C[i] = C[i - 1] + 1
+    else
+      C[i] = C[i - 1]
+    end
+  end
 end
