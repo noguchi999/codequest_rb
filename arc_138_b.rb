0c7bd465dfc6797856a4fab14180f02cd3f5c9be
noguchi999 <noguchi999@gmail.com>
2023-09-05 12:41:39 +0900

    [modify] ARC138 B

--- arc_138_b.rb
+++ arc_138_b.rb
@@ -4,4 +4,18 @@ if __FILE__ == $0
   A = gets.split.map(&:to_i)
 
   flip_count = 0
+  while 0 < A.size
+    if flip_count.even?
+      if A.last == 0
+        A.pop
+      elsif A.first == 0
+        A.shift
+        flip_count += 1
+      else
+        puts "No"
+        exit
+      end
+    end
+    flip_count += 1
+  end
 end
