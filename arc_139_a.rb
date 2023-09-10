81fde1186e19843f840d5eb350b4b92fc1aba0f1
noguchi999 <noguchi999@gmail.com>
2023-09-10 15:10:44 +0900

    [modify] ARC139 A

--- arc_139_a.rb
+++ arc_139_a.rb
@@ -4,4 +4,10 @@ if __FILE__ == $0
   T = [0] + gets.split.map(&:to_i)
 
   A = Array.new(N+1, 0)
+  (1..N).each do |i|
+    k = (A[i-1] / (2**T[i-1])) / 2 + 1
+    A[i] = 2**T[i] + 2**(T[i]+1) * k
+  end
+
+  puts A[N]
 end
