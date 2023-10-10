30996f1ca5658584a92389d45dfea922c1669cb9
noguchi999 <noguchi999@gmail.com>
2023-10-10 15:54:45 +0900

    [modify] ABC226 D

--- abc_226_d.rb
+++ abc_226_d.rb
@@ -3,7 +3,7 @@ if __FILE__ == $0
   N = gets.to_i
 
   _p = []
-  magic = []
+  magic = Set.new
   N.times do |i|
     x, y = gets.split.map(&:to_i)
     _p << [x, y]
@@ -14,6 +14,14 @@ if __FILE__ == $0
       next if s == e
       xs, ys = _p[s][0], _p[s][1]
       xe, ye = _p[e][0], _p[e][1]
+
+      x_inc = xe - xs
+      y_inc = ye - ys
+
+      d = x_inc.gcd(y_inc)
+      magic << [x_inc / d, y_inc / d]
     end
   end
+
+  puts magic.size
 end
