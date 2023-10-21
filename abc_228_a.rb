f959d9bd7dbd967b554a09fe441d16616ab258c8
noguchi999 <noguchi999@gmail.com>
2023-10-21 10:15:54 +0900

    [modify] ABC228 A

--- abc_228_a.rb
+++ abc_228_a.rb
@@ -1,4 +1,18 @@
 # ABC228 A
 if __FILE__ == $0
   S, T, X = gets.split.map(&:to_i)
+
+  if S < T
+    if S <= X && X < T
+      puts "Yes"
+    else
+      puts "No"
+    end
+  else
+    if X < T || S <= X
+      puts "Yes"
+    else
+      puts "No"
+    end
+  end
 end
