233d45415e1a70a49de470fb50ed29b2df0e9055
noguchi999 <noguchi999@gmail.com>
2023-11-07 16:24:37 +0900

    [modify] ABC229 A

--- abc_229_a.rb
+++ abc_229_a.rb
@@ -2,4 +2,12 @@
 if __FILE__ == $0
   S1 = gets.chomp
   S2 = gets.chomp
+
+  if S1 == "#." && S2 == ".#"
+    puts "No"
+  elsif S1 == ".#" && S2 == "#."
+    puts "No"
+  else
+    puts "Yes"
+  end
 end
