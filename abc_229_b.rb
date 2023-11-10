534db947561f24a5642b25d57641c71299b74965
noguchi999 <noguchi999@gmail.com>
2023-11-10 16:48:59 +0900

    [modify] ABC229 B

--- abc_229_b.rb
+++ abc_229_b.rb
@@ -6,4 +6,14 @@ if __FILE__ == $0
   _b.reverse!
 
   keta = [_a.length, _b.length].min
+  keta.times do |i|
+    a_int = _a[i].to_i
+    b_int = _b[i].to_i
+    if 10 <= a_int + b_int
+      puts "Hard"
+      exit
+    end
+  end
+
+  puts "Easy"
 end
