fdf8c999ef5b215a9160b787fdd758a1cceaccbf
noguchi999 <noguchi999@gmail.com>
2023-11-12 07:54:42 +0900

    [modify] ABC229 E

--- abc_229_e.rb
+++ abc_229_e.rb
@@ -2,4 +2,28 @@
 require 'union_find'
 if __FILE__ == $0
   N, M = gets.split.map(&:to_i)
+
+  edge = Array,new(N, [])
+  M.times do |i|
+    A, B = gets.split.map(&:to_i)
+    edge[A] << B
+  end
+
+  union_find = UnionFind.new(N)
+  ans = Array.new(N, 0)
+  count = 0
+  N.times.reverse do |i|
+    count += 1
+    edge[i].each do |x|
+      unless union_find.same?(i, x)
+        union_find.merge(i, x)
+        count -= 1
+      end
+    end
+    ans[i-1] = count
+  end
+
+  (1..N).each do |i|
+    puts ans[i]
+  end
 end
