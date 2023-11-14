541fcbe6ee46ef09a4cdad68c14ea537a6daeba5
noguchi999 <noguchi999@gmail.com>
2023-11-14 16:00:59 +0900

    [modify] ABC230 A

--- abc_230_a.rb
+++ abc_230_a.rb
@@ -1,4 +1,17 @@
 # ABC230 A
 if __FILE__ == $0
   N = gets.to_i
+  if N < 42
+    if N.to_s.length == 1
+      puts "AGC00#{N}"
+    else
+      puts "AGC0#{N}"
+    end
+  else
+    if N.to_s.length == 1
+      puts "AGC00#{N}"
+    else
+      puts "AGC0#{N}"
+    end
+  end
 end
