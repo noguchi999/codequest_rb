016a3489c8db1817ae92f0fccfff4f307179e6eb
noguchi999 <noguchi999@gmail.com>
2023-11-17 17:21:21 +0900

    [modify] ABC230 B

--- abc_230_b.rb
+++ abc_230_b.rb
@@ -3,4 +3,11 @@ if __FILE__ == $0
   S = gets.chomp
   N = S.length
   T = "oxx" * (10**5)
+  (T.length - N).times do |i|
+    if T[i..i+N] == S
+      puts "Yes"
+      exit
+    end
+  end
+  puts "No"
 end
