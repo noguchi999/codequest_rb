3bb1af5eff7f4cd6735b1c230c66f2105f3e8f5d
noguchi999 <noguchi999@gmail.com>
2023-11-26 07:06:07 +0900

    [modify] ABC230 C

--- abc_230_c.rb
+++ abc_230_c.rb
@@ -12,4 +12,23 @@ if __FILE__ == $0
   left_down_row, left_down_col = A + k2R, B - k2R
 
   ans = Array.new(Q-P+1+1) { Array.new(S-R+1+1, ".") }
+  (P..Q).each do |i|
+    (R..S).each do |j|
+      if i.between?(left_up_row, right_down_row) && j.between?(left_up_col, right_down_col)
+        if i - left_up_row == j - left_up_col
+          ans[i-P][j-R] = "#"
+        end
+      end
+
+      if i.between?(right_up_row, left_down_row) && j.between?(right_up_col, left_down_col)
+        if left_down_row - i == j - right_up_col
+          ans[i-P][j-R] = "#"
+        end
+      end
+    end
+  end
+
+  (1..Q-P+1).each do |i|
+    puts ans[i].join
+  end
 end
