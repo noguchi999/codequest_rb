295817b3c42622f992718e859d728165d7e166d1
noguchi999 <noguchi999@gmail.com>
2023-11-30 15:37:25 +0900

    [modify] ABC230 E

--- abc_230_e.rb
+++ abc_230_e.rb
@@ -8,4 +8,10 @@ if __FILE__ == $0
   (1..n_root).each do |k|
     ans += k * ((N / k).to_i - (N / (k + 1).to_i))
   end
+
+  (1..(N / n_root).to_i).each do |i|
+    ans += (N / i).to_i
+  end
+
+  puts ans
 end
