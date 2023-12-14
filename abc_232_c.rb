6eeec58cd4828dc0617763738db335fd85cf553e
noguchi999 <noguchi999@gmail.com>
2023-12-14 16:37:31 +0900

    [modify] ABC232 C

--- abc_232_c.rb
+++ abc_232_c.rb
@@ -3,4 +3,10 @@ if __FILE__ == $0
   N, M = gets.chomp.split.map(&:to_i)
 
   takahashi = Array.new(N+1) { Array.new(N+1, false) }
+  M.times do |i|
+    A, B = gets.chomp.split.map(&:to_i)
+    takahashi[A][B] = true
+    takahashi[B][A] = true
+  end
+
 end
