14d8dd338ecf028001dace53d1e37aed50a4f924
noguchi999 <noguchi999@gmail.com>
2023-12-22 15:05:02 +0900

    [modify] ABC232 D

--- abc_232_d.rb
+++ abc_232_d.rb
@@ -5,5 +5,9 @@ if __FILE__ == $0
   grid = []
   H.times do |i|
     ci = gets.chomp.split.map(&:to_i)
+    ci_list = Array.new(ci)
+    grid << ci_list
   end
+
+  grid_count = Array.new(H) { Array.new(W, 0) }
 end
