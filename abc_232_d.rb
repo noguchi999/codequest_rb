48fca269779973395d711e426de961a5b023198e
noguchi999 <noguchi999@gmail.com>
2023-12-25 15:56:18 +0900

    [modify] ABC232 D

--- abc_232_d.rb
+++ abc_232_d.rb
@@ -17,5 +17,21 @@ if __FILE__ == $0
   while !queue.empty?
     g, r = queue.shift
     now_count = grid_count[g][r]
+
+    if g + 1 < H && grid[g+1][r] == "."
+      if grid_count[g+1][r] == 0 < now_count + 1
+        grid_count[g+1][r] = now_count + 1
+        queue << [g+1, r]
+      end
+    end
+
+    if r + 1 < W && grid[g][r+1] == "."
+      if grid_count[g][r+1] == 0 < now_count + 1
+        grid_count[g][r+1] = now_count + 1
+        queue << [g, r+1]
+      end
+    end
   end
+
+  ans = 0
 end
