f999da5dc3a71cf1398da21407150db3a63bb207
noguchi999 <noguchi999@gmail.com>
2023-12-28 16:18:17 +0900

    [modify] ABC233 A

--- abc_233_a.rb
+++ abc_233_a.rb
@@ -1,4 +1,13 @@
 # ABC233 A
 if __FILE__ == $0
   X, Y = gets.split.map(&:to_i)
+  if Y <= X
+    puts 0
+  else
+    if (Y-X)%10 == 0
+      puts (Y-X)/10
+    else
+      puts (Y-X)/10 + 1
+    end
+  end
 end
