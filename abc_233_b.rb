4c75c4260c8a4e173e0681c8def1bf12deaba678
noguchi999 <noguchi999@gmail.com>
2023-12-30 07:42:41 +0900

    [modify] ABC233 A

--- abc_233_b.rb
+++ abc_233_b.rb
@@ -1,4 +1,10 @@
 # ABC233 A
 if __FILE__ == $0
   L, R = gets.chomp.split.map(&:to_i)
+  S = gets
+
+  s_center = S[L-1..R-1]
+  s_center = s_center.reverse
+
+  puts S[..L-2] + s_center + S[R..]
 end
