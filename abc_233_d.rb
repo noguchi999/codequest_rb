2eac7d1473f77d31c4236e733a148cea1f6dd632
noguchi999 <noguchi999@gmail.com>
2024-01-02 06:11:59 +0900

    [modify] ABC233 D

--- abc_233_d.rb
+++ abc_233_d.rb
@@ -2,4 +2,9 @@
 if __FILE__ == $0
   N, K = gets.split.map(&:to_i)
   A = [0] + gets.split.map(&:to_i)
+
+  a_com = Array.new(N+1, 0)
+  (1..N).each do |i|
+    a_com[i] = a_com[i-1] + A[i]
+  end
 end
