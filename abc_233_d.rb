06a8dd8befe4795c05548a951759cb530662bc6f
noguchi999 <noguchi999@gmail.com>
2024-01-03 05:49:04 +0900

    [modify] ABC233 D

--- abc_233_d.rb
+++ abc_233_d.rb
@@ -7,4 +7,13 @@ if __FILE__ == $0
   (1..N).each do |i|
     a_com[i] = a_com[i-1] + A[i]
   end
+
+  count = Hash.new(0)
+  ans = 0
+  (1..N).each do |i|
+    count[a_com[i-1]] += 1
+    ans += count[a_com[i]-K]
+  end
+
+  puts ans
 end
