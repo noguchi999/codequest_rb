a49855f79ef731fee1b7dd0e77227915e9342bd0
noguchi999 <noguchi999@gmail.com>
2024-02-03 16:24:23 +0900

    [modify] ABC235 C

--- abc_235_c.rb
+++ abc_235_c.rb
@@ -8,10 +8,15 @@ if __FILE__ == $0
 
   N.times do |i|
     count[a[i]] += 1
-    index[a[i]][count[a[i]]] = i + 1
+    index["#{a[i]]}_#{[count[a[i]]}"] = i + 1
   end
 
   Q.times do |i|
     x, k = gets.split.map(&:to_i)
+    if index["#{x}_#{k}"] == 0
+      puts -1
+    else
+      puts index["#{x}_#{k}"]
+    end
   end
 end
