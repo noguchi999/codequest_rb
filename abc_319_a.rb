85dadc5da8f6ecc858255e2975b4632d1007bc82
noguchi999 <noguchi999@gmail.com>
2024-02-18 06:48:58 +0900

    [modify] ABC319 A

--- abc_319_a.rb
+++ abc_319_a.rb
@@ -5,5 +5,11 @@ if __FILE__ == $0
     puts 3858
   elsif s == "ksun48"
     puts 3679
+  elsif s == "Benq"
+    puts 3658
+  elsif s == "Um_nik"
+    puts 3648
+  elsif s == "apiad"
+    puts 3638
   end
 end
